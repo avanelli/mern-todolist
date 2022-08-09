6569dd5c2da57c0cbaec6997c78da6448e80bc86
Andrea Vanelli <andrea.vanelli@gmail.com>
2022-08-09 18:33:20 +0200

    basic app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+ 
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import TodoList from "./components/todoList";
+import TodoEdit from "./components/todoEdit";
+import TodoCreate from "./components/todoCreate";
+ 
+const App = () => {
+ return (
+   <div>
+     <Navbar />
+     <Routes>
+       <Route exact path="/" element={<TodoList />} />
+       <Route path="/edit/:id" element={<TodoEdit />} />
+       <Route path="/create" element={<TodoCreate />} />
+     </Routes>
+   </div>
+ );
+};
+ 
 export default App;
