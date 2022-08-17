44c2f4f5b76b0cbd495231dc0d9d25ae3857ae90
Andrea Vanelli <andrea.vanelli@gmail.com>
2022-08-17 12:45:58 +0200

    fix db connect

--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -8,19 +8,10 @@ const apiPath = '/api/todo/v1'
 app.use(apiPath, todoapiv1)
 
 beforeAll(async () => {
-  process.env.ATLAS_URI = global.__MONGO_URI__
-  process.env.ATLAS_DB = 'todo'
-  await dbo.connectToServer(function (err) {
-    if (err) console.error(err)
-  })
+  await dbo.connectToServer()
 })
 
-/*
-afterAll(async () => {
-  await dbo.close()
-  })
-*/
-describe('Test Routes', function () {
+describe('Test Todo Routes', function () {
   /* test(`responds to POST ${apiPath}`, async () => {
     const res = await (await request(app).post(apiPath)).send({ payload })
     expect(res.header['content-type']).toBe('application/json; charset=utf-8')
@@ -28,7 +19,7 @@ describe('Test Routes', function () {
     expect(res.text).toEqual('hello world!')
   }) */
 
-  test('responds to GET /', async () => {
+  test('responds empty array to GET /', async () => {
     const res = await request(app).get(apiPath)
     expect(res.header['content-type']).toBe('application/json; charset=utf-8')
     expect(res.statusCode).toBe(200)
