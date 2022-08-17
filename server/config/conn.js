36f869ce7c4d42b69efd789309b65f9717d21099
Andrea Vanelli <andrea.vanelli@gmail.com>
2022-08-17 10:12:29 +0200

    add connection timeout

--- server/config/conn.js
+++ server/config/conn.js
@@ -2,20 +2,17 @@ const { MongoClient } = require('mongodb')
 const Db = process.env.ATLAS_URI
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  connectTimeoutMS: process.env.DB_CONNECT_TIMEOUT,
+  serverSelectionTimeoutMS: process.env.DB_CONNECT_TIMEOUT
 })
 
 let _db
 
 async function connectToServer () {
-  try {
-    // Use connect method to connect to the Server
-    await client.connect()
-    _db = client.db(process.env.ATLAS_DB)
-    console.log('Successfully connected to MongoDB.')
-  } catch (err) {
-    console.log(err.stack)
-  }
+  // Use connect method to connect to the Server
+  await client.connect()
+  _db = client.db(process.env.ATLAS_DB)
 }
 
 function getDb () {
