0cf7def47c04956f690d066b4cea696b324cece9
Andrea Vanelli <andrea.vanelli@gmail.com>
2022-08-18 22:13:15 +0200

    add inset - edit test

--- server/tests/todo-routes.test.js
+++ server/tests/todo-routes.test.js
@@ -1,4 +1,4 @@
-const bodyParser = require('body-parser')
+/* const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 app.use(bodyParser.json())
@@ -7,9 +7,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const dbo = require('../config/conn')
 const request = require('supertest')
 const todoapiv1 = require('../controllers/todoController')
-
+*/
+const request = require('supertest')
+const dbo = require('../config/conn')
 const apiPath = '/api/todo/v1'
-app.use(apiPath, todoapiv1)
+const app = require('../app')
 
 beforeAll(async () => {
   await dbo.connectToServer()
@@ -30,18 +32,44 @@ describe('Test Todo REST routes', function () {
       .send(payload)
       .expect('Content-Type', /json/)
       .expect(200)
-    console.log(res.body)
-    expect(res.body.insertedId).toEqual('hello world!')
-  })
+    expect(res.body).toEqual(expect.objectContaining({
+      insertedId: expect.any(String)
+    }))
 
-  test('responds empty array to GET /', async () => {
-    const res = await request(app).get(apiPath)
+    // read the inserted record
+    const res2 = await request(app).get(apiPath)
       .expect('Content-Type', /json/)
       .expect(200)
 
-    expect(res.text).toEqual('[]')
+    expect(res2.body[0]._id).toEqual(res.body.insertedId)
   })
 
+  test(`responds to POST ${apiPath}/:id updating a record`, async () => {
+    // read first record
+    const res2 = await request(app).get(apiPath)
+      .expect('Content-Type', /json/)
+      .expect(200)
+
+    // POST new data
+    const payload = { title: 'test changed title', content: 'test content', level: 'Mid', dueDate: '2022-09-01T16:17:14.000Z' }
+    const res = await request(app).post(apiPath + '/' + res2.body[0]._id)
+      .set('Accept', 'application/json')
+      .send(payload)
+      .expect('Content-Type', /json/)
+      .expect(200)
+    expect(res.body).toEqual(expect.objectContaining({
+      modifiedCount: 1,
+      matchedCount: 1
+    }))
+  })
+
+  test('db error', async () => {
+    dbo.setDb('')
+    const res = await request(app).get(apiPath)
+      .expect('Content-Type', /json/)
+      .expect(200)
+    expect(res.text).toEqual('[]')
+  })
   /*
   /insert
   post
