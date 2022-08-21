6d8d448e48d22939736b6962a1385976a0ac2731
Andrea Vanelli <andrea.vanelli@gmail.com>
2022-08-21 17:51:28 +0200

    add: custom error handler

--- server/app.js
+++ server/app.js
@@ -5,5 +5,10 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use('/api/todo/v1', require('./controllers/todoController'))
+app.use((err, req, res, next) => {
+  console.error('Path: ', req.path)
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 module.exports = app
