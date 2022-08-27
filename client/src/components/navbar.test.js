5ad792ba0d15c2a835f5445915bc6b05a8822dc6
Andrea Vanelli <andrea.vanelli@gmail.com>
2022-08-27 19:53:47 +0200

    wip

---
+++ client/src/components/navbar.test.js
@@ -0,0 +1,17 @@
+import { unmountComponentAtNode } from 'react-dom'
+
+let container = null
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
+// TODO follow https://it.reactjs.org/docs/testing-recipes.html
